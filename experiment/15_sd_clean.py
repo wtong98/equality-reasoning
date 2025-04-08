Fix root d scaling

experiment/15_sd_clean.py
@@ -225,12 +225,13 @@ g = sns.heatmap(mdf, vmin=0.5, vmax=1, square=False)
 g.figure.set_size_inches(3.5, 2.7)
 
 xs = 2**np.linspace(-5, 8)
-g.plot(xs, 20 - 2 * xs + 7, color='black', linestyle='dashed')
+# g.plot(xs, 20 - 2 * xs + 7, color='black', linestyle='dashed')
+g.plot(xs, 10 - 1 * xs + 8, color='black', linestyle='dashed')
 
 g.set_xlabel('Input dimension ($d$)')
 g.set_ylabel('# symbols ($L$)')
 
-g.figure.savefig('fig/ccn/lazy_ndim_v_nsym.svg')
+# g.figure.savefig('fig/ccn/lazy_ndim_v_nsym.svg')
 
 # <codecell>
 mdf = plot_df.copy()

experiment/6_toy_sd.py
@@ -425,20 +425,20 @@ plt.savefig('fig/noise_sweep_diff_sig_best_by_sig_gap.png')
 
 # <codecell>
 # NOTE: dimension dependence seems to enter when considering patch sizes > 2
-n_dims = 900
-n_points = 334
+n_dims = 92
+n_points = 74
 # n_points = np.round(0.5 * n_dims * np.log(n_dims)).astype(int)
 # n_points = n_dims
 # n_hidden = 892
-n_hidden = 1024
+n_hidden = 4096
 
 noise = 0
 
-gamma0 = 1e-5 * np.sqrt(n_dims)
+gamma0 = 1e-5
 # gamma0 = 1
 # gamma = gamma0 * np.sqrt(n_hidden)
 gamma = gamma0
-lr = gamma0**2 * 0.1
+lr = gamma0**2 * 0.1 * n_dims
 
 n_patches = 2
 
@@ -473,7 +473,7 @@ state, hist = train(config,
                     test_iter=iter(test_task), 
                     loss='bce',
                     test_every=1000,
-                    train_iters=5_000,
+                    train_iters=100_000,
                     # lr=1e-3,
                     # optim=sign_sgd,
                     optim=optax.sgd,

experiment/remote/15_sd_clean/lazy_var_symbols/run.py
@@ -52,8 +52,9 @@ for log10_gamma0, n_dims, n_vocab in zip(log10_gs, [n_dims_lazy, n_dims_rich], [
         noise = sig2
 
         gamma0 = 10**log10_gamma0
-        gamma = gamma0 * np.sqrt(d)
-        lr = gamma**2 * base_lr
+        # gamma = gamma0 * np.sqrt(d)
+        gamma = gamma0
+        lr = gamma**2 * base_lr * n_dims
 
         all_cases.append(
             Case(rf'$\gamma=10^{ {log10_gamma0} }$',

model/mlp.py
@@ -65,7 +65,7 @@ class MLP(nn.Module):
                                kernel_init=mup_init,
                                name=name)(x)
                 # x = np.sqrt(self.config.n_hidden / x.shape[-1]) * x
-                # x = np.sqrt(1 / x.shape[-1]) * x
+                x = np.sqrt(1 / x.shape[-1]) * x
 
             else:
                 x = nn.Dense(self.config.n_hidden, 