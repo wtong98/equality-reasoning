436fb869afa5f12db808ee47e9f11faf7f0019f4
William Tong <williamtong105@gmail.com>
2025-04-09 09:58:45 -0400

    Prepare revised noise run

--- experiment/remote/15_sd_clean/noise/sb_noise.sh
+++ experiment/remote/15_sd_clean/noise/sb_noise.sh
@@ -3,13 +3,13 @@
 #SBATCH -t 1-00:00:00
 #SBATCH -p kempner_requeue
 #SBATCH --gres=gpu:1
-#SBATCH --mem=256000
+#SBATCH --mem=64000
 #SBATCH -o log.%A.%a.out
 #SBATCH -e log.%A.%a.err
 #SBATCH --array=1-36
 #SBATCH --mail-type=END
 #SBATCH --mail-user=wtong@g.harvard.edu
-#SBATCH --account=kempner_pehlevan_lab
+#SBATCH --account=kempner_grads
 #SBATCH --exclude=holygpu8a19604
 
 source ../../../../../venv_haystack/bin/activate
